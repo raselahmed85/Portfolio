58bd88b38c70e2046ca59e2dd7c27769b0a2983c
raselahmed85 <mr.raselahmed85@gmail.com>
2025-02-27 23:59:52 +0600

    update commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+
+const form = document.querySelector("form");
+
+let name = document.querySelector("#name")
+let email = document.querySelector("#email")
+let number = document.querySelector("#numbre")
+let massege = document.querySelector("#massege")
+
+
+form.addEventListener("submit", mysubmitFunction)
+
+function mysubmitFunction(e){
+    e.preventDefault()
+
+    console.log(name.value);
+    console.log(email.value);
+    console.log(number.value);
+    console.log(massege.value);
+}
